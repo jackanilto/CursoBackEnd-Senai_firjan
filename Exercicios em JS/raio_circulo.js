d62120885630934cd4b1eb11340641d7711f108c
Janilto Oliveira <janiltojack@yahoo.com.br>
2025-08-23 18:21:49 -0300

    Area x Raio de um circulo

---
+++ Exercicios em JS/raio_circulo.js
@@ -0,0 +1,11 @@
+// Valor de PI
+const pi = 3.14159;
+
+// Raio do círculo
+let raio = 55;
+
+// Calculo da área do círculo
+let area = pi * raio * raio;
+
+// Exibe o resultado no console
+console.log("Raio " + raio + " Área do Circulo: " + area);
