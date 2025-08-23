efdfe0ceb3ec26009e4bd0ade3dc521d70bcb9e9
Janilto Oliveira <janiltojack@yahoo.com.br>
2025-08-23 18:32:01 -0300

    Update Calcula_nota_media.js

--- Exercicios em JS/Calcula_nota_media.js
+++ Exercicios em JS/Calcula_nota_media.js
@@ -7,7 +7,7 @@ let nota3 = 90;
 let media = (nota1 + nota2 + nota3) / 3;
 
 // Exibir média
-console.log("A média do aluno é: " + media); // .toFixed(2) limita a 2 casas decimais
+console.log("A média do aluno é: " + media); // .toFixed(2) Para limitar a 2 casas decimais
 
 // Verifica se aprovado ou reprovado
 if (media >= 7) {
