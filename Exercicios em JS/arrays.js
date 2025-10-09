fbce08e248f7aa446926ce4ebf94e8769bd55368
Janilto Oliveira <janiltojack@yahoo.com.br>
2025-10-09 20:37:14 -0300

    Salvando Exercício

--- Exercicios em JS/arrays.js
+++ Exercicios em JS/arrays.js
@@ -25,10 +25,85 @@
 
 
 // com While =====================================================
-let frutas = ['maçã', 'banana', 'laranja', 'uva']
-let i = 0
+// let frutas = ['maçã', 'banana', 'laranja', 'uva']
+// let i = 0
 
-while (i < frutas.length) {
-    console.log(`Fruta ${i + 1}: ${frutas[i]}`)
-    i++ // incrementa o contador para evitar loop infinito
-}
+// while (i < frutas.length) {
+//     console.log(`Fruta ${i + 1}: ${frutas[i]}`)
+//     i++ // incrementa o contador para evitar loop infinito
+// }
+
+
+
+//Usando MAP
+// let idades = [10, 15, 20, 25, 30]
+// // Cria um novo array com o dobro das idades
+// let idadesDobradas = idades.map(idade => idade * 2)
+
+// console.log('Idades originais:', idades)
+// console.log('Idades dobradas:', idadesDobradas)
+
+
+// USANDO FOREACH ================================================
+// let idades = [10, 15, 20, 25, 30]
+// let idadesDobradas = [] // novo array vazio
+// // Percorre o array e adiciona o dobro de cada idade
+// idades.forEach(idade => {
+//     idadesDobradas.push(idade * 2)
+// })
+
+// console.log('Idades originais:', idades)
+// console.log('Idades dobradas:', idadesDobradas)
+
+
+
+
+// var alunos = ['ana', 'bruno', 'carla', 'daniel', 'eduarda']
+
+// // Cria um novo array com os nomes em maiúsculas
+// var alunosMaiusculos = alunos.map(function(aluno){
+//     return alunos.map
+// })
+
+// console.log('Nomes originais:', alunos)
+// console.log('Nomes em maiúsculas:', alunosMaiusculos)
+
+
+
+// Usando Arrow Function =======================================================
+var filmes = ['Lagoa Azul', 'Rambo III', 'O Cubo', 'Outro', 'Mais Um']
+filmes.forEach(filme => console.log('Eu gosto do filme', filme))
+
+
+console.clear()
+
+
+
+let precos = [30, 135, 140, 10.50, 89, 190]
+
+
+let precosComDesconto = precos.map(preco => {
+    if (preco > 100) {
+        return (preco * 0.85).toFixed(2) 
+    } else {
+        return preco.toFixed(2)
+    }
+})
+
+console.log('Preços Normais:', precos)
+console.log('Preços com desconto:', precosComDesconto)
+
+
+//========================================================
+
+console.clear()
+
+var preco = [30, 150, 10.50]
+var newPrecos = precos.map(function(preco) {
+    if (preco>100){
+        return ((preco * 0.85).toFixed(2))
+    }else {
+        return preco.toFixed(2)
+    }
+})
+console.log('Novos Preços:', newPrecos)
